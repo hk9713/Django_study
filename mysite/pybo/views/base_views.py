cd00334a49d4d817d236b29ba4ef0d4ff5fea0f9
hk9713 <96482380+hk9713@users.noreply.github.com>
2024-02-02 15:21:05 +0900

    Feat : 검색

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,11 +1,21 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 
 from ..models import Question
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '') # 검색어
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     try:
         pages = paginator.page(page)
@@ -13,7 +23,7 @@ def index(request):
         pages = paginator.page(1)
     except EmptyPage:
         pages = paginator.page(1)
-    context = {'question_list': pages}
+    context = {'question_list': pages, 'page':page, 'kw':kw}
     return render(request, 'pybo/question_list.html', context)
 
 def detail(request, question_id):
